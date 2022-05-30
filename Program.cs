632f68da4288b08052a00e37d52401db629295ef
KozlovNN <kozlovnn.19@gmail.com>
2022-05-30 19:04:16 +0300

    Calc

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿double a;
+double b;
+double total;
+char oper;
+
+Console.WriteLine("Введите первое число:");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите оператор");
+oper = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+b = Convert.ToDouble(Console.ReadLine());
+
+if (oper == '+')
+{
+    total = a + b;
+    Console.WriteLine(a + " + " + b + " = " + total);
+}
+
+else if (oper == '-')
+{
+    total = a - b;
+    Console.WriteLine(a + " - " + b + " = " + total);
+}
+
+else if (oper == '*')
+{
+    total = a * b;
+    Console.WriteLine(a + " * " + b + " = " + total);
+}
+
+else if (oper == '/')
+{
+    total = a / b;
+    Console.WriteLine(a + " / " + b + " = " + total);
+}
